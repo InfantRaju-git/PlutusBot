11f95f0056d357f2644ed4e700b06313fd55b92c
infantraju123@gmail.com <infantraju123@gmail.com>
2024-06-16 21:05:27 +0530

    bot changes

--- Bot.py
+++ Bot.py
@@ -13,21 +13,18 @@ def run_trade_bot(symbol):
     start_time = datetime.time(9, 15)
     end_time = datetime.time(15, 30)
 
-    print("Security Extraction Job: 10:00:00")
+    print("Security Extraction Job: 9:35:00")
     schedule.every().day.at(datetime.time(9, 35, 00).strftime("%H:%M:%S")).do(DhanMethods.filter_and_save_csv).tag('securityid')
     
-    print("Security Extraction Job: 10:10:00")
+    print("Security Extraction Job: 0:42:00")
     schedule.every().day.at(datetime.time(9, 42, 00).strftime("%H:%M:%S")).do(BotMethods.set_config, symbol=symbol).tag('setConfig')
     
-    jobCount = 0
     for hour in range(start_time.hour, end_time.hour + 1):
         for minute in range(start_time.minute, 60, Global.SYMBOL_SETTINGS[symbol]["TRADE_TF"]):
             schedule_time = datetime.time(hour, minute, 5)
             if start_time <= schedule_time < end_time:
-                if jobCount != 0:
-                    print("Scheduling Trade Job", schedule_time)
-                    schedule.every().day.at(schedule_time.strftime("%H:%M:%S")).do(BotMethods.trade_symbol, symbol=symbol).tag('trade')
-                jobCount += 1
+                print("Scheduling Trade Job", schedule_time)
+                schedule.every().day.at(schedule_time.strftime("%H:%M:%S")).do(BotMethods.trade_symbol, symbol=symbol).tag('trade')
                     
     print("Exit Trade Job: 3:29:00")
     schedule.every().day.at(datetime.time(15, 29, 0).strftime("%H:%M:%S")).do(BotMethods.exit_open_trade, symbol=symbol).tag('exittrade')
